9cab175474e768a5ed12700197cade8ff7639655
Peter Berry <pwberry@gmail.com>
2012-06-22 21:16:15 +0100

    Allow for optional fields
    Field cases in |consolidate| now return Maybe, so no result can still be
    a success (for optional fields).

--- Handler/Call.hs
+++ Handler/Call.hs
@@ -6,7 +6,7 @@ import EveApiTypes
 import EveApiValues
 import ModelUtils
 
-import Data.Maybe (isNothing, fromJust)
+import Data.Maybe (isNothing, fromJust, catMaybes)
 import qualified Data.Text as T
 import qualified Data.Map as M
 import Data.Traversable (sequence)
@@ -73,28 +73,36 @@ callForm scope call callargs = do
                     _ -> formFail []
         argfields :: AForm App App [APIArgument]
         argfields = formToAForm $ do
-            let consolidate ::
-                       MForm App App [(FormResult  a , [FieldView App App])]
-                    -> MForm App App  (FormResult [a], [FieldView App App])
+            let consolidate :: -- Collect results, if there's no error
+                       MForm App App [(FormResult (Maybe a), [FieldView App App])]
+                    -> MForm App App  (FormResult       [a], [FieldView App App])
                 consolidate mresults = do
                     results <- mresults
-                    let allresults = mconcat $ map (fmap return . fst) results
+                    let allresults = fmap catMaybes . mconcat .
+                                        map (fmap return . fst) $ results
                         allfields = concatMap snd results
                     return (allresults, allfields)
             consolidate $ forM args $ \(Arg name _atype opt) -> do
                 let req = opt == Mandatory  -- Check later in case opt == FromKey
                     notImplemented = return (FormFailure ["Not implemented"], [])
+                    -- Do the following:
+                    -- * Use mopt or mreq as appropriate
+                    -- * If there's a result, pass it to a postprocessing
+                    --   function, which may return failure
+                    -- * If this function succeeds, apply the provided constructor
+                    -- * Put the fieldview in a list for later consolidation
                     mkfield :: (b -> APIArgument)
                                   -> Field App App a
                                   -> FieldSettings App
                                   -> Maybe a
-                                  -> (Maybe a -> FormResult b) -- post-processing
-                                  -> MForm App App (FormResult APIArgument,
+                                  -> (Maybe a -> FormResult (Maybe b))
+                                  -> MForm App App (FormResult (Maybe APIArgument),
                                                     [FieldView App App])
                     mkfield constr fieldtype settings def process = do
                         (result, field) <- moptreq req fieldtype settings def
                         case result of
-                            FormSuccess a  -> return (constr <$> process a, [field])
+                            FormSuccess a  -> return (fmap constr <$> process a,
+                                                                            [field])
                             FormMissing    -> return (FormMissing,          [field])
                             FormFailure ms -> return (FormFailure ms,       [field])
                     parseListField :: ([a] -> APIArgument)  -- ^ constructor
@@ -102,13 +110,14 @@ callForm scope call callargs = do
                                            -> Text          -- ^ error message
                                            -> Parser a      -- ^ element parser
                                            -> Maybe Text    -- ^ default
-                                           -> MForm App App (FormResult APIArgument,
-                                                             [FieldView App App])
+                                           -> MForm App App
+                                                (FormResult (Maybe APIArgument),
+                                                 [FieldView App App])
                     parseListField constr settings errmsg parser def
                         = mkfield constr textField settings def $ \rawlist ->
                             let maybeElems = parseList parser =<< rawlist
                             in  maybe (FormFailure [errmsg])
-                                      (\elems -> FormSuccess elems)
+                                      (\elems -> FormSuccess $ Just elems)
                                       maybeElems
                 case name of
                     ArgTIDs -> parseListField ArgIDs
@@ -121,22 +130,24 @@ callForm scope call callargs = do
                                    checkBoxField
                                    "Include members"
                                    Nothing $ FormSuccess .
-                                    (maybe 0 $ \include ->
-                                        if include then 0 else 1)
+                                    (maybe Nothing $ \include ->
+                                        if include then Nothing else Just 1)
                                             -- alliance list, 1 = omit member corps
                     ArgTNames -> parseListField ArgNames
                                     "List of names (comma-separated)"
                                     "Invalid name list"
                                     characterName
                                     Nothing
-                    ArgTCharacterID -> notImplemented   -- int
-                    ArgTCorporationID -> notImplemented -- int
+                    ArgTCharacterID -> notImplemented   -- int, may be FromKey
+                    ArgTCorporationID -> notImplemented -- int, may be FromKey
                     ArgTEventIDs -> parseListField ArgIDs
                                     "List of event IDs (comma-separated)"
                                     "Invalid event ID list"
                                     decimal
                                     Nothing
-                    ArgTContractID -> notImplemented    -- int
+                    ArgTContractID ->
+                        mkfield ArgContractID intField "Contract ID" Nothing $
+                            maybe FormMissing (FormSuccess . Just)
                     ArgTBeforeKillID -> notImplemented  -- int
                     ArgTOrderID -> notImplemented       -- int
                     ArgTExtended -> notImplemented      -- for corp member tracking
